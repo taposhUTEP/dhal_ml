e2c7c64c842fa95d0418e2b34c7f91e3cb22494c
Taposh Kumer Sarker <tsarker@miners.utep.edu>
2023-11-27 14:00:25 -0700

    Logistic regression implemented

--- logistic_regression/main.py
+++ logistic_regression/main.py
@@ -1,9 +1,8 @@
-# Data Processing
 import pandas as pd
 import numpy as np
 
 # Modelling
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
 from scipy.stats import randint
@@ -20,10 +19,11 @@ X, Y = data_processing.get_x_y.get_now()
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=42)
 
-rf = RandomForestClassifier()
-rf.fit(X_train, y_train)
+lr = LogisticRegression()
 
-y_pred = rf.predict(X_test)
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_test)
 
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
